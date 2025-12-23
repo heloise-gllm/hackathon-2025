062f84f9288600ebe18390c9bd0771c9461153a1
Duriel7 <duriel@live.fr>
2025-12-23 12:34:24 +0100

    WIP feat: form partially created

--- src/features/goodies/manager/form-idea-new.tsx
+++ src/features/goodies/manager/form-idea-new.tsx
@@ -1,9 +1,18 @@
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useQueryClient } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useCanGoBack, useRouter } from '@tanstack/react-router';
 import { useForm } from 'react-hook-form';
 
 import { zFormFieldsIdea, zGoodieCategory } from '../schema';
+import { onError, onSuccess, ORPCError } from '@orpc/client';
+import { C } from 'vitest/dist/chunks/reporters.d.BFLkQcL6.js';
+import { orpc } from '@/lib/orpc/client';
+import { PreventNavigation } from '@/components/prevent-navigation';
+import { PageLayout, PageLayoutTopBar } from '@/layout/app/page-layout';
+import { B } from 'node_modules/better-auth/dist/index-COnelCGa.mjs';
+import { BackButton } from '@/components/back-button';
+import { Button } from '@react-email/components';
+import { Form } from '@/components/form';
 
 export const FormIdeaNew = () => {
   const router = useRouter();
@@ -17,4 +26,57 @@ export const FormIdeaNew = () => {
       description: '',
     },
   });
+
+  const goodieCreate = useMutation(
+    orpc.goodie.create.mutationOptions({
+      onSuccess: async () => {
+        await queryClient.invalidateQueries({
+          queryKey: orpc.goodie.getAll.key(),
+          type: 'all',
+        });
+
+        if (canGoBack) router.history.back({ ignoreBlocker: true });
+        else router.navigate({ to: '..', replace: true, ignoreBlocker: true });
+      },
+      onError: (error) => {
+        if (
+          error instanceof ORPCError &&
+          error.code === 'CONFLICT' &&
+          error.data.target.includes('name')
+        ) {
+          form.setError('name', { message: 'Name already exists' });
+          return;
+        }
+      },
+    })
+  );
+
+  return (
+    <>
+      <PreventNavigation shouldBlock={form.formState.isDirty} />
+      <Form
+        {...form}
+        onSubmit={async (values) => {
+          goodieCreate.mutate(values);
+        }}
+      >
+        <PageLayout>
+          <PageLayoutTopBar>
+            backButton={<BackButton />}
+            actions=
+            {
+              <Button
+                size="sm"
+                type="submit"
+                className="min-w-20"
+                loading={goodieCreate.isPending}
+              >
+                {"Ajouter l'idée"}
+              </Button>
+            }
+          </PageLayoutTopBar>
+        </PageLayout>
+      </Form>
+    </>
+  );
 };
